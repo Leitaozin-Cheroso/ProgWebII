8854ed9cf5d728c2ebff04352f48756689fcc251
Leitaozin-Cheroso <91471054+Leitaozin-Cheroso@users.noreply.github.com>
2021-12-21 15:36:58 -0300

    Update Atividade_02.js

--- Atividade_02.js
+++ Atividade_02.js
@@ -1,22 +1,22 @@
-const elementos = document.querySelectorAll('h2'); 
-const topicos = new Array();
-elementos.forEach(elemento => {
-	topicos.push(elemento.textContent);
-	const link = document.createElement('a'); 
-	link.setAttribute('name', elemento.textContent);
-	elemento.append(link);
-	const volta = document.createElement('a');
-	volta.setAttribute('href','#'); 
-	volta.textContent ='  -' + 'Voltar'; 
-	elemento.append(volta); 
-});
-lista = document.querySelector('ol'); 
-topicos.forEach(topico => { 
-	const item = document.createElement('li');
-	item.textContent = topico;
-	lista.append(item); 
-	const link = document.createElement('a'); 
-	link.setAttribute('href', `#${topico}`);   
-	link.textContent = ' - ' + topico; 
-	item.append(link);
-});
+        const elementos = document.querySelectorAll('h2'); 
+        const topicos = new Array();
+        elementos.forEach(elemento => {
+	        topicos.push(elemento.textContent);
+	        const link = document.createElement('a'); 
+	        link.setAttribute('name', elemento.textContent);
+	        elemento.append(link);
+	        const voltar = document.createElement('a');
+	        voltar.setAttribute('href','#'); 
+	        voltar.textContent ='  -' + 'Voltar'; 
+	        elemento.append(volta); 
+        });
+        lista = document.querySelector('ol'); 
+        topicos.forEach(topico => { 
+	        const item = document.createElement('li');
+	        item.textContent = topico;
+	        lista.append(item); 
+	        const link = document.createElement('a'); 
+	        link.setAttribute('href', `#${topico}`);   
+	        link.textContent = ' - ' + topico; 
+	        item.append(link);
+        });
