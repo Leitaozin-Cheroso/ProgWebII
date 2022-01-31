6e0c07e004491c8a6850aa2baf531c9eb2970ee2
Leitaozin-Cheroso <91471054+Leitaozin-Cheroso@users.noreply.github.com>
2022-01-31 10:07:15 -0300

    Create Tamanho.js

---
+++ Tamanho.js
@@ -0,0 +1,32 @@
+let tamanho = 3;
+function atualiza(){
+    const h1 = document.querySelector("h1");
+    h1.style.fontSize = `${tamanho}em`;
+
+    const h2 = document.querySelector("h2");
+    h2.style.fontSize = `${tamanho}em`;
+
+    const h3 = document.querySelector("h3");
+    h3.style.fontSize = `${tamanho}em`;
+
+    const h4 = document.querySelector("h4");
+    h4.style.fontSize = `${tamanho}em`;
+
+    const h5 = document.querySelector("h5");
+    h5.style.fontSize = `${tamanho}em`;
+
+    const h6 = document.querySelector("h6");
+    h6.style.fontSize = `${tamanho}em`;
+}
+function aumenta(){
+    tamanho += 1;
+    while (tamanho < 9){
+        atualiza()
+    }
+}
+function diminui(){
+    if (tamanho > 1){
+        tamanho -= 1;
+        atualiza()
+    }
+}
